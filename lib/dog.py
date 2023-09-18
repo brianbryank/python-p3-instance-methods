c68dda4f046a10ae287338479ff431103968efca
brianbryank <brianbryan0125@gmail.com>
2023-09-18 20:45:31 +0300

    passed

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,13 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+
+        # fido = Dog()
+        # fido.bark()
+
+    def sit(self):
+        print("The dog is sitting.")  
     pass
+
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,15 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print("Hello World!")
     #Instance method definition
+      
+        # todler = Person()
+        # todler.talk()
+    def walk(self):
+        print("The person is walking.")
+
+        # todler = Person()
+        # todler.walk()
     pass
